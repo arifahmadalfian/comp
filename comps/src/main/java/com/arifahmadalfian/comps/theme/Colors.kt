c060c77adabed5c3f131f9eb0e02b3c4614d99dd
Arief Ahmad Alfian <49640973+arifahmadalfian@users.noreply.github.com>
2022-08-27 20:45:43 +0700

    rename root project

--- comps/src/main/java/com/arifahmadalfian/comps/theme/Colors.kt
+++ comps/src/main/java/com/arifahmadalfian/comps/theme/Colors.kt
@@ -6,3 +6,5 @@ val BackgroundContent = Color(0xFFF5F5F5)
 val BlueButtonBottom = Color(0xFF0072FD)
 val BlueButtonTop = Color(0xFF367AF6)
 val ButtonBorder = Color(0x3000000)
+
+val Red600 = Color(0xFFD6001C)
